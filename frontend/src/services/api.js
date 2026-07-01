f7038f74fe264b3f171271848c717803a2eac385
JemimaNguess <jemimaoceanenguessan@gmail.com>
2026-07-01 02:58:27 +0000

    Production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,48 +1,15 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
 
-const getExpoHost = () => {
-  return (
-    Constants.manifest?.debuggerHost?.split(':')[0] ||
-    Constants.manifest2?.debuggerHost?.split(':')[0] ||
-    Constants.expoConfig?.hostUri?.split(':')[0] ||
-    Constants.expoConfig?.extra?.apiHost ||
-    Constants.manifest?.extra?.apiHost ||
-    null
-  );
-};
-
-const getApiHost = () => {
-  const expoHost = getExpoHost();
-  if (expoHost) {
-    return expoHost;
-  }
-
-  if (Platform.OS === 'android') {
-    return '10.0.2.2';
-  }
-
-  if (Platform.OS === 'ios') {
-    return '127.0.0.1';
-  }
-
-  return 'localhost';
-};
-
-const API_URL = Constants.manifest?.extra?.apiHost
-  ? `http://${Constants.manifest.extra.apiHost}/api`
-  : `http://${getApiHost()}:8000/api`;
-console.log('[API] baseURL=', API_URL, 'expoHost=', getExpoHost());
-// Si tu utilises un vrai téléphone, ajoute "apiHost" dans app.json extra ou remplace ici par l'IP de ton PC.
+const API_URL = 'http://10.135.16.189:8000/api';
+console.log('[API] baseURL=', API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
   timeout: 50000,
   headers: {
     'Content-Type': 'application/json',
-    'Accept':       'application/json',
+    'Accept': 'application/json',
   },
 });
 
